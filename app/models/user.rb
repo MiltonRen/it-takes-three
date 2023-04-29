8072389525b6f9583ad9f96046264c2d3851d780
Milton Ren <milton.x.ren@gmail.com>
2023-04-28 19:07:36 -0700

    fix: misc small issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 # profile string
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
